52cd2b841c11006a78ddbd8fb44f4c534721aa8a
inzamam nur <inzamamnur.14@gmail.com>
2022-10-25 15:49:23 +0600

    home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React from "react";
+import icon1 from "../../Assets/icon-1.png";
 
 const Home = () => {
-    return (
-        <div>
-            <h2>Home</h2>
+  return (
+    <div>
+      <div
+        className="hero bg-cover"
+        style={{
+          backgroundImage: `url("https://radiustheme.com/demo/wordpress/eikra/wp-content/uploads/2017/11/banner_image5.jpg?id=1219")`,
+        }}
+      >
+        <div className=""></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div
+            className=" "
+            style={{ marginTop: "150px", marginBottom: "150px" }}
+          >
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5 text-5xl">
+              Learn a new skill online on your time 57,000 Online Courses.
+            </p>
+            <button className="btn btn-primary">Start A Free TRial</button>
+          </div>
         </div>
-    );
+      </div>
+
+      {/* Student Review */}
+      <div
+        className="hero bg-cover"
+        style={{
+          backgroundImage: `url("https://radiustheme.com/demo/wordpress/eikra/wp-content/uploads/2017/11/background.jpg?id=1154")`,
+        }}
+      >
+        <div className=""></div>
+        <div  className="hero-content text-neutral-content ">
+          <div
+            className=" "
+            style={{ marginTop: "150px", marginBottom: "150px" }}
+          >
+            <h1 className="mb-5 text-5xl font-bold">Our Happy Students</h1>
+            <p   className=" mb-5 text-2xl">
+            Excellent content and assignments that build on your knowledge, reinforce, and then expand. I recently secured new employment using PHP and couldn't have done so without the Professional Web Development courses.
+            </p>
+            <h4>David Fahim</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
